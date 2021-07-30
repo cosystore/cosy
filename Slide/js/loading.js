89d1819be9cb81a63ab7789a1063ac97f8837919
Lê Khả Đức Anh <caubelamli@gmail.com>
2021-07-30 11:03:45 +0700

    xoa file loi

--- Slide/js/loading.js
+++
@@ -1,6 +0,0 @@
-var loader = document.getElementById("home-preloader");
-            window.addEventListener("load", function(){
-                setTimeout(() => {
-                    loader.style.display = "none";
-                }, 2000);
-            });
